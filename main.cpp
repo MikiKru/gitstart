b75982469897cc3f5d2803770f206cff15662da6
MikiKru <michal_kruczkowski@o2.pl>
2019-11-10 15:56:17 +0100

    zmiana w projekcie

--- main.cpp
+++ main.cpp
@@ -9,5 +9,7 @@ using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
